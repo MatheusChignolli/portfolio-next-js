79a889b7fcf00af04c46f89b3482f1ca6e3e4a75
Matheus Chignolli <matheuschignolli@gmail.com>
2024-02-16 08:16:13 -0300

    improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import githubIcon from "../../public/github.svg";
 import linkedinIcon from "../../public/linkedin.svg";
 import mailIcon from "../../public/mail.svg";
 import clickIcon from "../../public/click.svg";
+import reactIcon from "../../public/technologies/react.svg";
 
 const EmptyBox = () => {
   return <div className="bg-black" />;
@@ -16,20 +17,42 @@ export default function Home() {
           <h2 className="text-3xl">Software Engineer</h2>
           <h1 className="text-4xl">Matheus Chignolli</h1>
         </div>
-        <a
-          href="https://medium.com/@matheuschignolli"
-          target="_blank"
-          className="bg-white flex flex-col items-center justify-center"
-        >
-          <h3 className="text-2xl">Articles</h3>
-          <Image width={50} priority src={clickIcon} alt="Click icon" />
-        </a>
+        <div className="grid grid-cols-2 gap-1 h-full">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/matheus-chignolli-a0115b155/"
+            className="bg-white row-span-2 flex items-center justify-center"
+          >
+            <Image width={50} priority src={linkedinIcon} alt="LinkedIn icon" />
+          </a>
+          <a
+            href="https://github.com/MatheusChignolli"
+            target="_blank"
+            className="bg-white flex items-center justify-center"
+          >
+            <Image width={50} priority src={githubIcon} alt="Github icon" />
+          </a>
+          <a
+            href="mailto:matheuschignolli@gmail.com"
+            className="bg-white flex items-center justify-center"
+          >
+            <Image width={50} priority src={mailIcon} alt="Mail icon" />
+          </a>
+        </div>
         <a
           href="https://www.linkedin.com/in/matheus-chignolli-a0115b155/details/certifications/"
           target="_blank"
           className="bg-white flex flex-col items-center justify-center"
         >
           <h3 className="text-2xl">Certifications</h3>
+          <Image width={50} priority src={clickIcon} alt="Click icon" />
+        </a>
+        <a
+          href="https://medium.com/@matheuschignolli"
+          target="_blank"
+          className="bg-white flex flex-col items-center justify-center"
+        >
+          <h3 className="text-2xl">Articles</h3>
           <Image width={50} priority src={clickIcon} alt="Click icon" />
         </a>
         <a
@@ -40,35 +63,12 @@ export default function Home() {
           <h3 className="text-2xl">Posts</h3>
           <Image width={50} priority src={clickIcon} alt="Click icon" />
         </a>
-        <div className="grid grid-cols-2 gap-1 h-full">
-          <a
-            href="https://github.com/MatheusChignolli"
-            target="_blank"
-            className="bg-white flex items-center justify-center"
-          >
-            <Image width={50} priority src={githubIcon} alt="Github icon" />
-          </a>
-          <EmptyBox />
-          <a
-            href="mailto:matheuschignolli@gmail.com"
-            className="bg-white flex items-center justify-center"
-          >
-            <Image width={50} priority src={linkedinIcon} alt="LinkedIn icon" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/matheus-chignolli-a0115b155/"
-            target="_blank"
-            className="bg-white flex items-center justify-center"
-          >
-            <Image width={50} priority src={mailIcon} alt="Mail icon" />
-          </a>
-        </div>
         <div className="bg-white flex items-center justify-center">
           <h3 className="text-2xl">About {"->"}</h3>
         </div>
         <div className="bg-white col-span-5 flex items-center justify-start px-12">
           <p className="text-xl">
-            As a seasoned software developer, I thrive on diverse projects aimed
+            As a seasoned software engineer, I thrive on diverse projects aimed
             at solving real-world problems for clients. My focus is on creating
             meaningful solutions that positively impact users, regardless of
             scale. I prioritize continuous learning to enhance problem-solving
@@ -80,18 +80,38 @@ export default function Home() {
             development.
           </p>
         </div>
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
-
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
-        <EmptyBox />
+        <a
+          href="/projects"
+          className="bg-white col-span-2 flex flex-col items-center justify-center"
+        >
+          <h3 className="text-2xl">Projects</h3>
+          <Image width={50} priority src={clickIcon} alt="Click icon" />
+        </a>
+        <div className="bg-white col-span-2 flex items-center justify-center">
+          <h3 className="text-2xl">Technologies {"->"}</h3>
+        </div>
+        <div className="bg-white col-span-2 flex items-center justify-around text-xl">
+          <ul className="list-disc">
+            <li>Javascript</li>
+            <li>Typescript</li>
+            <li>NodeJs</li>
+            <li>ReactJs</li>
+            <li>Svelte</li>
+            <li>Flutter</li>
+            <li>Ruby on Rails</li>
+            <li>Unit/E2E tests</li>
+          </ul>
+          <ul className="list-disc">
+            <li>GraphQl</li>
+            <li>REST</li>
+            <li>AWS</li>
+            <li>MongoDB</li>
+            <li>Postgres</li>
+            <li>MySQL</li>
+            <li>Git</li>
+            <li>CI/CD</li>
+          </ul>
+        </div>
       </div>
     </main>
   );
