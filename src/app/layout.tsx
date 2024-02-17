ca9da149c5e6c784b3ec8b50338c3f7980d88608
Matheus Chignolli <matheuschignolli@gmail.com>
2024-02-17 11:36:49 -0300

    improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,10 @@ import "./globals.css";
 const inter = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Matheus Chignolli",
-  description: "Software Engineer portfolio",
-  applicationName: "Matheus Chignolli portfolio",
+  title: "Matheus Chignolli - Software Engineer Portfolio",
+  description:
+    "Matheus Chignolli's portfolio showcasing software engineering projects and expertise.",
+  applicationName: "Matheus Chignolli Portfolio",
   authors: [
     {
       name: "Matheus Chignolli",
@@ -16,35 +17,31 @@ export const metadata: Metadata = {
   ],
   creator: "Matheus Chignolli",
   keywords: [
-    "webdevelopment",
-    "web",
+    "web development",
     "react",
-    "reactjs",
-    "node",
-    "nodejs",
-    "graphql",
-    "rest",
-    "development",
-    "tech",
+    "node.js",
     "graphql",
     "portfolio",
-    "software",
-    "softwaredeveloper",
-    "softwareengineer",
+    "software engineering",
     "chignolli",
   ],
-
   openGraph: {
     type: "website",
     locale: "en_US",
     alternateLocale: "pt_BR",
     url: "https://chignolli.com",
-    siteName: "Matheus Chignolli",
+    siteName: "Matheus Chignolli Portfolio",
     countryName: "Brazil",
-    description: "Matheus Chignolli`s portfolio website",
+    description:
+      "Matheus Chignolli's portfolio website showcasing software engineering projects and expertise.",
     determiner: "the",
     emails: "matheuschignolli@gmail.com",
-    title: "Matheus Chignolli",
+    title: "Matheus Chignolli - Software Engineer Portfolio",
+    images: [
+      {
+        url: "https://media.licdn.com/dms/image/D5603AQFdUYVG2Z8p7Q/profile-displayphoto-shrink_800_800/0/1675702060704?e=1713398400&v=beta&t=tQij3it6jVusvH3FJQyUN_X_RC4__ou8yT8pVHtBRB4",
+      },
+    ],
   },
 };
 
