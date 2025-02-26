9f4b4d0b81e3fe8991910d9d50936a292b515a63
Matheus Chignolli <matheuschignolli@gmail.com>
2025-02-26 11:48:53 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,20 @@ export default function RootLayout({
     <html lang={params.lang} suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
+        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:site" content={metadata.twitter.site} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="twitter:image" content={metadata.twitter.image} />
         <script
           dangerouslySetInnerHTML={{
             __html: `
