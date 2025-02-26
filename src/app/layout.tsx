3d22c8d4ce3215547cfe0fddc630dd9e009925af
Matheus Chignolli <matheuschignolli@gmail.com>
2025-02-26 11:39:26 -0300

    add more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Fira_Code({ subsets: ["latin"] });
 
-const url = "https://chignolli.com"
+const url = "https://chignolli.com";
 
 export const metadata = {
   title: "Matheus Chignolli - Software Engineer Portfolio",
@@ -38,22 +38,32 @@ export const metadata = {
     description:
       "Matheus Chignolli's portfolio website showcasing software engineering projects and expertise.",
     determiner: "the",
-    emails: "matheuschignolli@gmail.com",
     title: "Matheus Chignolli - Software Engineer Portfolio",
     images: [
       {
         url: `${url}/cover-h.jpg`,
         width: 1200,
         height: 630,
-        alt: 'Capa Matheus Chignolli'
+        alt: 'Capa Matheus Chignolli',
       },
       {
         url: `${url}/cover-v.jpg`,
         width: 960,
         height: 1280,
-        alt: 'Capa Matheus Chignolli'
+        alt: 'Capa Matheus Chignolli',
       },
-    ]
+    ],
+    email: "matheuschignolli@gmail.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@MatheusChignolli",
+    creator: "@MatheusChignolli",
+    image: `${url}/cover-h.jpg`,
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
   metadataBase: new URL(url),
 };
